31796c1dd50efd05a52e486de2644f331bf7aaf7
Prostochell <dessenp1945@gmail.com>
2023-06-03 16:27:45 +0700

    LittleFix3

--- app/src/main/java/com/examle/ftrackercomposerefactor/ui/elements/MainActivity.kt
+++ app/src/main/java/com/examle/ftrackercomposerefactor/ui/elements/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.examle.ftrackercomposerefactor.FallWorkingDirectory.SensorTrack
 import com.examle.ftrackercomposerefactor.FallWorkingDirectory.TrackDelper
 import com.examle.ftrackercomposerefactor.ui.theme.FtrackerComposeRefactorTheme
@@ -34,11 +39,12 @@ class MainActivity : ComponentActivity() {
                 ) {
                     isOnboardingPassed.value?.let {
                         if (it) {
-
+                            MainUI()
                         } else {
                             OnBoarding {
                                 mainViewModel.setOnboardingCompeted()
                             }
+
                         }
                     }
                 }
--- app/src/main/java/com/examle/ftrackercomposerefactor/ui/elements/MainPage.kt
+++ app/src/main/java/com/examle/ftrackercomposerefactor/ui/elements/MainPage.kt
@@ -1,2 +1,43 @@
 package com.examle.ftrackercomposerefactor.ui.elements
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.Role.Companion.Switch
+import androidx.compose.ui.unit.dp
+
+var label = "Test"
+@Composable
+public fun MainUI() {
+    var switchOn by remember {
+        mutableStateOf(false)
+    }
+    Text(text = label)
+    Spacer(modifier = Modifier.padding(start = 8.dp))
+    Switch(checked = switchOn,
+        onCheckedChange = { switchOn_ ->
+            switchOn = switchOn_})
+}
+@Composable
+fun Switch(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {}
+
+
+
+
